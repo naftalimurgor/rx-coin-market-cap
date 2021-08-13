3ff2a54d4fcf3e1d6b611ab54ab8a7dc808045c1
naftalimurgor <murgornaftali@gmail.com>
2021-08-13 23:24:37 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const axios = require("axios");
+const dotenv = require('dotenv')
+
+// get env secrets
+dotenv.config()
+const app = express();
+
+const CMC_API_KEY = process.env.CMC_API_KEY
+
+async function getCoinListing() {
+  const headers = {
+    "X-CMC_PRO_API_KEY": CMC_API_KEY,
+  };
+  
+  const listing = await axios.default.get(
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    { headers }
+  );
+  return listing.data;
+}
+
+app.get("/crypto/listing", async (req, res) => {
+  const listing = await getCoinListing();
+  res.status(200).json({ msg: "success", data: listing });
+});
+
+http.Server(app).listen(8000);
