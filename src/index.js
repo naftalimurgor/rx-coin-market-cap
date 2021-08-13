63d52ec5332ee39d59c299cfbef5c326f7ca3e37
naftalimurgor <murgornaftali@gmail.com>
2021-08-14 00:44:50 +0300

    add cors support

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const http = require("http");
 const axios = require("axios");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
+const cors = require('cors')
 
 // get env secrets
-dotenv.config()
+dotenv.config();
 const app = express();
+app.use(cors())
 
-const CMC_API_KEY = process.env.CMC_API_KEY
+
+const CMC_API_KEY = process.env.CMC_API_KEY;
 
 async function getCoinListing() {
   const headers = {
     "X-CMC_PRO_API_KEY": CMC_API_KEY,
   };
-  
+
   const listing = await axios.default.get(
     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
     { headers }
